d7c4d097cb3910488f8f0c6e6ce8732e4e53f5d2
Md Ahsan <ahsan041220012gmail.com>
2025-08-05 01:30:27 +0530

    Type casting and varibale

---
+++ p4.py
@@ -0,0 +1,6 @@
+# this is single line comments for tesing
+print("Hello Raja Jaani")
+"""
+this is for multiple line comment in python
+"""
+print("yeah Multiple comments is also working")
---
+++ p5.py
@@ -0,0 +1,16 @@
+# How write variable in python
+a = 10
+b = 2
+print(a+b)
+
+# Type casting
+
+a = str(10)
+b = int(10)
+c = float(10)
+print(a,b,c)
+
+# change type casting
+a = 10
+b = "Raja jaani"
+print(type(a),type(b))
