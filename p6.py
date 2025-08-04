a02690b5ec75440e27a73929747e3974c10a73b2
Md Ahsan <ahsan041220012gmail.com>
2025-08-05 01:58:13 +0530

    Unpacking collection and many values

---
+++ p6.py
@@ -0,0 +1,12 @@
+# Many Values to Multiple Variables
+x,y,z = ["one","two","three"]
+print(x,y,z)
+
+# One Value to Multiple Variables
+x = y = z = "Raja jaani"
+print(x,y,z)
+
+# unpack collection
+Fruits = ["Apple","Mango","Banana"]
+x, y, z = Fruits
+print(x,y,z)
